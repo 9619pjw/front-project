007445842fccc1684eacc98529ea3b0a5570ad83
JokerBear_96 <9619pjw@gmail.com>
2024-01-22 17:35:08 +0900

    feat: add email validation

--- modal.js
+++ modal.js
@@ -3,8 +3,19 @@ document.addEventListener('DOMContentLoaded', function () {
     var btn = document.querySelector('.subscribe-button');
     var closeBtn = document.querySelector('.close-modal');
 
-    btn.onclick = function() {
-        modal.style.display = 'block';
+    var emailInput = document.querySelector('.enter-email-box input[type="email"]');
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    // 이메일 유효성 검증
+    btn.onclick = function(event) {
+        var email = emailInput.value;
+
+        if(email === '' || !emailPattern.test(email)) {
+            alert("이메일 형식이 올바르지 않습니다. 다시 입력해주세요.");
+            event.preventDefault();
+        } else {
+            modal.style.display = 'block';
+        }
     }
 
     // 모달 내부 닫기 버튼 클릭 이벤트
